2b701e534ae193edc2e70e377de6fa3d96fbaca1
Mesut yiğit <yigit.mesut@outlook.com>
2026-05-23 21:57:08 +0300

    Repository yazıldı

---
+++ Elastic.BlogWeb/Repository/BlogRepository.cs
@@ -0,0 +1,30 @@
+﻿using Elastic.BlogWeb.Models;
+using Elastic.Clients.Elasticsearch;
+
+namespace Elastic.BlogWeb.Repository
+{
+    public class BlogRepository
+    {
+        private readonly ElasticsearchClient _client;
+        private const string indexName = "blogposts";
+
+        public BlogRepository(ElasticsearchClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<Blog?> SaveAsync(Blog blog)
+        {
+            blog.Created = DateTime.Now;
+
+            var response = await _client.IndexAsync(blog, x => x.Index(indexName));
+
+            if (!response.IsValidResponse) return null;
+   
+
+            blog.Id = response.Id;
+            return blog;
+        }
+    }
+}
+
