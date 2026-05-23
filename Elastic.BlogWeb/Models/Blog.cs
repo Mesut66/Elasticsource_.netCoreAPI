491f01050a9b167d8fbfdde144360c972ea00113
Mesut yiğit <yigit.mesut@outlook.com>
2026-05-23 20:53:16 +0300

    Elastic Blog Uygulaması

---
+++ Elastic.BlogWeb/Models/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Elastic.BlogWeb.Models
+{
+    public class Blog
+    {
+        //Buradaki isimler ES de yaptığımız mapping ile aynı olmalıdır.Json propery ile aynı yaptım
+        [JsonPropertyName("_id")]
+        public string Id { get; set; } = null!;
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = null!;
+        [JsonPropertyName("content")]
+        public string Content { get; set; } = null!;
+        [JsonPropertyName("tags")]
+        public string[] Tags { get; set; } = null!;
+        [JsonPropertyName("user_id")]
+        public Guid UserId { get; set; }
+        [JsonPropertyName("created")]
+        public DateTime Created { get; set; }
+    }
+}
---
+++ Elastic.BlogWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Elastic.BlogWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
